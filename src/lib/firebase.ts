9ac05e31d6b27d41276130897758f98741159efd
AnshumanTiwari2006 <anshumantiwarirenukoot@gmail.com>
2026-01-09 17:40:39 +0530

    New V5 with full backend and API patching and removed env as well

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAeIPDvbxcUCzsN5vE_f96PukpitjIHAR0",
-  authDomain: "scholar-synergy.firebaseapp.com",
-  projectId: "scholar-synergy",
-  storageBucket: "scholar-synergy.firebasestorage.app",
-  messagingSenderId: "626942309974",
-  appId: "1:626942309974:web:87e6ec3235633ff76dedf7"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
